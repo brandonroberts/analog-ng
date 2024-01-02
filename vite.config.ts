3805330de437b23d3f979a6a458492a1a31aa143
Brandon Roberts <robertsbt@gmail.com>
2024-01-02 09:13:39 -0600

    feat: update example components

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import analog from '@analogjs/platform';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   publicDir: 'src/assets',
+  optimizeDeps:{ 
+    include: ['@angular/router']
+  },
   build: {
     target: ['es2020'],
   },
